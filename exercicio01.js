2979534276aa4d3346ed4bf13cce23d39dc0faaa
nathalia <nacaldas@hotmail.com>
2022-04-14 20:19:33 -0300

    adicionando exercicio 01

---
+++ exercicio01.js
@@ -0,0 +1,23 @@
+// 1 - Elabore um algoritmo que receba um número
+// (1-7) e devolva o dia da semana correspondente.
+
+let diaDaSemana = 4
+switch (diaDaSemana) {
+    case 1:
+        console.log('domingo')
+    case 2:
+        console.log('segunda')
+    case 3:
+        console.log('terça')
+    case 4:
+        console.log('quarta')
+    case 5:
+        console.log('quinta')
+    case 6:
+        console.log('sexta')
+    case 7:
+        console.log('sábado')
+        default: 
+        console.log('não é um número válido')
+}
+
