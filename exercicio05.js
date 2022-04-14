9532753aaebfa621de603941363487952acc96f2
nathalia <nacaldas@hotmail.com>
2022-04-14 20:26:06 -0300

    adicionando exercicio05.js

---
+++ exercicio05.js
@@ -0,0 +1,12 @@
+//  5 - Crie uma função que recebe o ano de nascimento 
+// da pessoa informando se ela é maior de idade ou menor.
+
+function receberAno(ano){
+    if (ano > 2004){
+        return 'Você é menor de idade'
+    }else{
+        return "Você é maior de idade"
+    }
+}
+
+console.log(receberAno(2004))
