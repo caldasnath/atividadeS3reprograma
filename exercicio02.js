824d2cc66ce92317b2cba4ce169b0262878e4e5f
nathalia <nacaldas@hotmail.com>
2022-04-14 20:21:36 -0300

    adicionando exercicio 02

---
+++ exercicio02.js
@@ -0,0 +1,29 @@
+// 2 - Elabore um algoritmo que receba dois números e determine qual
+// é o maior entre eles, se os números forem iguais, mostre 
+// uma mensagem no console "Os números são iguais".
+
+
+//function calculandoMaiorEMenor(valorA, valorB) {
+//  if (valorA == valorB) {
+//    console.log('Os valores são iguais')
+//} else if (valorA > valorB) {
+//   console.log('O valor A é maior que B ')
+// } else {
+//   console.log('O valor B é maior que A')
+//}
+//}
+
+//calculandoMaiorEMenor(1, 1)
+
+let valorA = 10
+let valorB = 5
+if (valorA > valorB) {
+    console.log(valorA + ' É maior que ' + valorB)
+} else if (valorB > valorA) {
+    console.log(valorB + ' É maior que ' + valorA)
+} else {
+    console.log('Os números são iguais')
+}
+
+
+
